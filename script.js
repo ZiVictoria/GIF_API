ded9304b1e0da3a31403c4fa61140188398be7da
ZiVictoria <viktoria.zaviyska@gmail.com>
2022-10-22 04:48:03 +0300

    add error processing

--- script.js
+++ script.js
@@ -8,11 +8,24 @@ function searchGif() {
     fetch("https://api.giphy.com/v1/gifs/search?api_key=D50JTA6ZqdOsssLYlNtUTLiqTsCVEy5T&q=" + valueInput + "&limit=5")
         .then(response => response.json())
         .then(data => {
-            let output = document.querySelector(".result");
-            output.innerHTML = "";
-            for (let i in data.data) {
-                output.innerHTML += `
-                <img src="${data.data[i].images.fixed_height.url} alt="gif">`;
-            }
-        });
+            let output = data.data;
+           getData(output);
+        })
+        .catch(error => alert( `Sorry:( The server is currently unable to handle the incoming request. Error: ${error.name} - ${error.message}`))
     }
+
+    function getData(output) {
+        let result = document.querySelector(".result");
+        result.innerHTML = "";
+        try {
+            for (let i in output) {
+                result.innerHTML += `
+                <img src="${output[i].images.fixed_height.url} alt="gif">`;
+            
+        } 
+        } catch (error) {
+            alert("Aw, Snap! Data loading failed.");
+        }  
+    }
+    
+    
